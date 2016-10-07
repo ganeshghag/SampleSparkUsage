f9beca83cefda93b05f3d692de7fa243ba88f940
Ganesh Ghag <ganesh.ghag@gmail.com>
2016-10-07 16:57:42 +0530

    added total count of letters, example

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 this is a good opp
 this is very nice
 every dog has  his day
-a penny for your thougts"# SampleSparkUsage" 
+a penny for your thougts
---
+++ src/main/java/com/ghag/rnd/analytics/SampleSparkUsage/SampleSpark2.java
@@ -0,0 +1,29 @@
+package com.ghag.rnd.analytics.SampleSparkUsage;
+
+
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+public class SampleSpark2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("from inside sample spark 1");
+		String logFile = "readme.md"; // Should be some file on
+														// your system
+		SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> lines = sc.textFile(logFile).cache();
+
+		JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+		int totalLength = lineLengths.reduce((a, b) -> a + b);
+		
+		System.out.println("total length="+totalLength);
+		
+
+	}
+
+}
